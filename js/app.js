2033084cfedf7170472ae929704641fa14ec4b39
kneazle <kneazle@yandex.ru>
2018-07-22 19:51:15 +0300

    Player moves by arrow keys in game area

--- js/app.js
+++ js/app.js
@@ -24,13 +24,50 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // The image/sprite for our hero, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    //initial position for the hero
+    this.x = 2;
+    this.y = 5;
+};
 
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 10);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'up':
+            if (this.y != 0)
+                --this.y;   
+            break;
+        case 'down':
+            if (this.y != 5)
+                ++this.y;
+            break;
+        case 'left':
+            if (this.x != 0)
+                --this.x;
+            break;
+        case 'right':
+            if (this.x != 4)
+            ++this.x;
+            break;
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+let allEnemies = [];
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
